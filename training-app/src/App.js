cba3d664ff7b4088178572ecee9f72478c7c5501
Your NameRichard <richard.sieber1109@hotmail.com>
2022-03-25 17:35:39 +0100

    start

---
+++ training-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Trainingsheet from './components/Trainingsheet';
+
+
+function App() {
+  
+  return (
+      <div>
+        <Trainingsheet />
+      </div>
+    
+        
+   
+  )
+}
+
+export default App;
